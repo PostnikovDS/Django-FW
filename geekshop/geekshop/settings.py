a54679685c4affef699c9c6689fec32a81035713
Helmar <postnikovden@mail.ru>
2022-01-20 23:58:44 +0500

    lesson-3

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3_old',
     }
 }
 
@@ -120,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (BASE_DIR/'static',)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
